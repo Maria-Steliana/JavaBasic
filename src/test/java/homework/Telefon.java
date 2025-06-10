084627af80a6a2fa0ea886de286d834b4248cf25
Maria-Steliana <maya_dlr03@yahoo.com>
2025-06-11 00:01:58 +0300

    start working

--- src/test/java/homework/Telefon.java
+++ src/test/java/homework/Telefon.java
@@ -1,6 +1,6 @@
 package homework;
 
-public class Telefon {
+public abstract class Telefon {
     // de facut un exemplu OOP asemanator cu exemplul de la curs;
     // trebuie folosite; mostenirea, incapsularea, polimorfism, abstractizare(clase abstracte si interfete)
 }
